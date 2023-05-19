94143a113a0f936cafb59d3041e40bca9aa049b2
André Loezer <andre.loezer@etools.com.br>
2023-05-19 19:17:05 -0300

    +FileUpload

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,32 @@
 import 'dotenv/config'
+import { resolve } from 'node:path'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(require('@fastify/multipart'))
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
+
 app.register(jwt, {
   secret: 'spacetime',
 })
+
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
